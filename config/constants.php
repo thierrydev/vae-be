c5659ade7503d4a170eb4b1dcb014524760279c5
tdesk <dev@perif.co>
2024-10-09 10:47:47 +0200

    Remove unused constants

--- config/constants.php
+++ config/constants.php
@@ -6,7 +6,4 @@ return [
     'CUSTOMER_TYPES' => ['INDIVIDUAL', 'BUSINESS'],
     'USER_ROLES' => ['ADMIN','SUPPORT','USER'],
     'PAID_DATE_FORMAT' => 'date_format:Y-m-d H:i:s',
-    'ATTEMPTS_STATUS' => ['ANSWERED', 'UNANSWERED', 'IGNORED'],
-    'ATTEMPTS_METHOD' => ['PHONE', 'EMAIL', 'MAIL'],
-
 ];
